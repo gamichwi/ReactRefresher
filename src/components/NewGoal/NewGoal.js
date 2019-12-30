dcb03ed394cef6f3bc2c18a3496e0f0814818de3
Gavin Williams <gamichwi@gmail.com>
2019-12-30 18:24:14 +1100

    improved the new goal adding by using usesetstate

--- src/components/NewGoal/NewGoal.js
+++ src/components/NewGoal/NewGoal.js
@@ -1,20 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./NewGoal.css";
 const NewGoal = props => {
+  const [enteredText, setEnteredText] = useState('');
+
   const addGoalHandler = event => {
     event.preventDefault();
 
-    const newGoal = {   
+    const newGoal = {
       id: Math.random().toString(),
-      text: "My New Goal!"
+      text: enteredText
     };
 
     props.onAddGoal(newGoal);
   };
 
+  const textChangeHandler = event => {
+    setEnteredText(event.target.value);
+  };
   return (
     <form className="new-goal" onSubmit={addGoalHandler}>
-      <input type="text" />
+      <input type="text" onChange={textChangeHandler} />
       <button type="submit"> Add Goal</button>
     </form>
   );
