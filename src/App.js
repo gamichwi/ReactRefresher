7ba2c235aaec7125c02865c94105ea783e778ced
Gavin Williams <gamichwi@gmail.com>
2019-12-30 17:39:21 +1100

    goal list is an array that is mapped in the GoalList component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from "react";
-import GoalList from './components/GoalList';
-import './App.css'
+import GoalList from "./components/GoalList";
+import "./App.css";
 
 const App = () => {
+  const courseGoals = [
+    { id: "cg1", text: "Finish the Course" },
+    { id: "cg2", text: "Learn all about the Course Main Topic" },
+    { id: "cg3", text: "Help other students in the Course Q&A" },
+    { id: "cg4", text: "Bootstrap my final project" }
+  ];
+
   return (
     <div className="course-goals">
       <h2> Course Goals</h2>
-      <GoalList />
+      <GoalList goals = {courseGoals}/>
     </div>
   );
 };
